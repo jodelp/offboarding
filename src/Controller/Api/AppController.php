eaec6bfcd1454c64e990b568793e02213cf7b844
jodelp <jodelp@sonarlogic.biz>
2023-01-26 08:12:12 +0800

    MYA-1049: Workbench: Staff productivity should be displayed upon ClockIn of the staff /api/summary_productivity_report.json

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -32,9 +32,25 @@ use Cake\Datasource\ConnectionManager;
 class AppController extends Controller
 {
     const CONNECTION_NAMES = ['cs_cib' => 'Staff Central', 'mystaff' => 'Mystaff'];
+
+    const TIMEZONE_CODE_AU_ACT = 'AU-ACT';
+    const TIMEZONE_CODE_AN_NSW = 'AU-NSW';
+    const TIMEZONE_CODE_AU_NT = 'AU-NT';
+    const TIMEZONE_CODE_AU_QLD = 'AU-QLD';
+    const TIMEZONE_CODE_AU_SA = 'AU-SA';
+    const TIMEZONE_CODE_AU_TAS = 'AU-TAS';
+    const TIMEZONE_CODE_AU_VIC = 'AU-VIC';
+    const TIMEZONE_CODE_AU_WA = 'AU-WA';
+    const TIMEZONE_CODE_CA = 'CA';
+    const TIMEZONE_CODE_CN = 'CN';
+    const TIMEZONE_CODE_DE = 'DE';
+    const TIMEZONE_CODE_HK = 'HK';
+    const TIMEZONE_CODE_NZ = 'NZ';
     const TIMEZONE_CODE_PH = 'PH';
-    const TIMEZONE_CODE_US = 'US';
+    const TIMEZONE_CODE_SG = 'SG';
     const TIMEZONE_CODE_UK = 'UK';
+    const TIMEZONE_CODE_US = 'US';
+    const TIMEZONE_CODE_US_NJ = 'US-NJ';
 
     /**
      * Token
@@ -171,6 +187,66 @@ class AppController extends Controller
                 $tz = 'America/New_York';
                 break;
             case self::TIMEZONE_CODE_PH:
+            default:
+                $tz = 'Asia/Manila';
+                break;
+        }
+
+        switch (strtoupper($tzCode)) {
+            case self::TIMEZONE_CODE_AU_ACT;
+                $tz = 'Australia/Lindeman';
+                break;
+            case self::TIMEZONE_CODE_AN_NSW;
+                $tz = 'Australia/Sydney';
+                break;
+            case self::TIMEZONE_CODE_AU_NT;
+                $tz = 'Australia/Darwin';
+                break;
+            case self::TIMEZONE_CODE_AU_QLD;
+                $tz = 'Australia/Brisbane';
+                break;
+            case self::TIMEZONE_CODE_AU_SA;
+                $tz = 'Australia/Adelaide';
+                break;
+            case self::TIMEZONE_CODE_AU_TAS;
+                $tz = 'Australia/Currie ';
+                break;
+            case self::TIMEZONE_CODE_AU_VIC;
+                $tz = 'Australia/Melbourne';
+                break;
+            case self::TIMEZONE_CODE_AU_WA;
+                $tz = 'Australia/Perth';
+                break;
+            case self::TIMEZONE_CODE_CA;
+                $tz = 'America/Toronto';
+                break;
+            case self::TIMEZONE_CODE_CN;
+                $tz = 'Asia/Hong_Kong';
+                break;
+            case self::TIMEZONE_CODE_DE;
+                $tz = 'Europe/Berlin';
+                break;
+            case self::TIMEZONE_CODE_HK;
+                $tz = 'Asia/Hong_Kong';
+                break;
+            case self::TIMEZONE_CODE_NZ;
+                $tz = 'Pacific/Tarawa';
+                break;
+            case self::TIMEZONE_CODE_PH;
+                $tz = 'Asia/Manila';
+                break;
+            case self::TIMEZONE_CODE_SG;
+                $tz = 'Asia/Singapore';
+                break;
+            case self::TIMEZONE_CODE_UK;
+                $tz = 'Europe/London';
+                break;
+            case self::TIMEZONE_CODE_US;
+                $tz = 'America/New_York';
+                break;
+            case self::TIMEZONE_CODE_US_NJ;
+                $tz = 'America/New_York';
+                break;
             default:
                 $tz = 'Asia/Manila';
                 break;
